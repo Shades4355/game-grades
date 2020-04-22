0fb7b3f37beb9dd52658600b396f20d86505483a
Pat Walsh <pmwals09@gmail.com>
2020-04-22 16:49:21 -0400

    Added model validations, removed comments/unused line of code

--- app/javascript/react/components/GameTile.test.js
+++ app/javascript/react/components/GameTile.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Enzyme, { mount } from "enzyme"
 import Adapter from "enzyme-adapter-react-16"
-import { BrowserRouter } from 'react-router-dom'
 import GameTile from './GameTile'
 
 Enzyme.configure({ adapter: new Adapter() })
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ApplicationRecord
-  
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :player_num, presence: true
 end
