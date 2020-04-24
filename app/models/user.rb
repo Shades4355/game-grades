92230a17540bc64584940c4c6a394bb58035b071
Pat Walsh <pmwals09@gmail.com>
2020-04-24 15:15:45 -0400

    Added CarrierWave with local storage to add profile photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :email, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+  mount_uploader :profile_photo, ProfilePhotoUploader
 end
