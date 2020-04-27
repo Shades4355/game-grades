bb745782f080439be9065d4c55d177501008ebfb
jhayes00 <jason.hayes2@gmail.com>
2020-04-27 11:34:25 -0400

    reviews index with model tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,6 @@ class Review < ApplicationRecord
     belongs_to :game
     
     validates :rating, :body, presence: true
+    validates :rating, numericality: true
+    validates :rating, inclusion: {in: 0..5}
 end
