d3a2729312d6e07ad40ffc9d09d35721bd4e6624
jhayes00 <jason.hayes2@gmail.com>
2020-04-28 15:39:59 -0400

    cleaned up routes.rb comments

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :games, only: [:index, :show, :create] #do
+      resources :games, only: [:index, :show, :create]
       resources :reviews, only: [:create]
-      # end
     end
   end
 
